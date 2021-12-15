12f96bb129d26d80ca6673f28a5605efcd58308e
Fernando Goulart <fernando.goulart@olxbr.com>
2021-12-15 17:11:12 -0300

    removes authentication

--- cleanup.js
+++ cleanup.js
@@ -1,14 +1,7 @@
 const core = require('@actions/core');
-const Authentication = require('./Authentication.js');
 const CacheOperation = require('./CacheOperation.js');
 const fs = require('fs');
-
-const access_id = core.getInput('aws-id');
-const secret_key = core.getInput('aws-key');
-const region = core.getInput('aws-region');
-const role = core.getInput('aws-role');
-const roleSessionName = core.getInput('aws-role-session-name');
-const externalId = core.getInput('aws-role-external-id');
+const AWS = require('aws-sdk');
 
 const bucket_root = core.getInput('s3-bucket-root');
 const filename = core.getInput('zip-filename');
@@ -16,43 +9,21 @@ const bucket_dir = core.getInput('bucket-dir');
 const cache_key = core.getInput('cache-key');
 const dir_to_cache = core.getInput('dir-to-cache');
 
-const auth = new Authentication(access_id, secret_key, region)
-
-function cacheCleanUp(connection) {
-    try {
-        var operation = core.getState("operation");
-        console.log(`OPERATION ${operation}`);
-        if (operation == 'creation') {
-            const cacheOperation = new CacheOperation(connection, bucket_root, bucket_dir, cache_key, filename, dir_to_cache);
-            cacheOperation.generateCache().then((result) => {
-                core.info('Cache generation succeded');
-                auth.logout();
-            }, function (err) {
-                core.error(err);
-                auth.logout();
-                core.setFailed(err);
-            })
-        } else {
-            core.info('Cache was found. No need for a new cache creation.');
-            auth.logout();
-        }
-    } catch (err) {
-        core.error(err);
-        auth.logout();
-        core.setFailed(err);
-    }
-}
-
 try {
-    if (access_id != '') {
-        auth.login(role, roleSessionName, externalId).then((connection) => {
-            cacheCleanUp(connection)
+    var operation = core.getState("operation");
+    console.log(`OPERATION ${operation}`);
+    if (operation == 'creation') {
+        const cacheOperation = new CacheOperation(AWS, bucket_root, bucket_dir, cache_key, filename, dir_to_cache);
+        cacheOperation.generateCache().then((result) => {
+            core.info('Cache generation succeded');
+        }, function (err) {
+            core.error(err);
+            core.setFailed(err);
         })
     } else {
-        connection = Authentication.emptyConnector()
+        core.info('Cache was found. No need for a new cache creation.');
     }
-} catch (error) {
-    core.error(error);
-    auth.logout();
-    core.setFailed(error);
+} catch (err) {
+    core.error(err);
+    core.setFailed(err);
 }
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
+const AWS = require('aws-sdk');
 const core = require('@actions/core');
 const github = require('@actions/github');
-const Authentication = require('./Authentication.js');
 const CacheOperation = require('./CacheOperation.js');
 const process = require('process');
 
 try {
-
-  const access_id = core.getInput('aws-id');
-  const secret_key = core.getInput('aws-key');
-  const region = core.getInput('aws-region');
-  const role = core.getInput('aws-role');
-  const roleSessionName = core.getInput('aws-role-session-name');
-  const externalId = core.getInput('aws-role-external-id');
-
   const bucket_root = core.getInput('s3-bucket-root');
   const filename = core.getInput('zip-filename');
   const bucket_dir = core.getInput('bucket-dir');
@@ -20,35 +12,20 @@ try {
   const dir_to_cache = core.getInput('dir-to-cache');
   const dir_to_unzip = core.getInput('dir-to-unzip');
 
-
-  function cacheAction(connection) {
-    const cacheOperation = new CacheOperation(connection, bucket_root, bucket_dir, cache_key, filename, dir_to_cache, dir_to_unzip);
-    cacheOperation.retrieveCache().then((result) => {
-      console.log(`RESULT = ${result.operation}`);
-      core.debug(`RESULT = ${result.operation}`);
-      core.saveState("operation", result.operation);
-      console.log('Save State operation OK');
-      core.setOutput('operation', result.operation);
-    }, function (err) {
-      core.error(err);
-      core.saveState("operation", "failed")
-      core.setFailed(err);
-      process.exit(1);
-    })
-  }
-
-  const auth = new Authentication(access_id, secret_key, region)
-  console.log(access_id);
-  if (access_id != '' && access_id) {
-    auth.login(role, roleSessionName, externalId).then((connection) => {
-      cacheAction(connection);
-    })
-  } else {
-    cacheAction(Authentication.emptyConnector());
-  }
+  const cacheOperation = new CacheOperation(AWS, bucket_root, bucket_dir, cache_key, filename, dir_to_cache, dir_to_unzip);
+  cacheOperation.retrieveCache().then((result) => {
+    console.log(`RESULT = ${result.operation}`);
+    core.debug(`RESULT = ${result.operation}`);
+    core.saveState("operation", result.operation);
+    console.log('Save State operation OK');
+    core.setOutput('operation', result.operation);
+  }, function (err) {
+    core.error(err);
+    core.saveState("operation", "failed")
+    core.setFailed(err);
+  })
 } catch (error) {
   core.saveState("operation", "failed")
   core.setFailed(error);
   console.log(error);
-  process.exit(1);
 }
